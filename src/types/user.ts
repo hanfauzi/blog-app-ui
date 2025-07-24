9512c33d72d155ace2469561da83722e032e4e01
hanfauzi <nifauzi45@gmail.com>
2025-07-24 15:59:55 +0700

    feat: add login (#1)

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,16 +1,7 @@
 export interface User {
-  lastLogin: number;
-  userStatus: "ENABLED" | "DISABLED" | string;
-  created: number;
-  accountType: "BACKENDLESS" | string;
-  socialAccount: string | null;
-  ownerId: string;
-  oAuthIdentities: any;
-  name: string;
-  ___class: string;
-  blUserLocale: string;
-  "user-token": string;
-  updated: number;
-  email: string;
-  objectId: string;
+id: number;
+name: string;
+email: string;
+createdAt: Date;
+updatedAt: Date;
 }
